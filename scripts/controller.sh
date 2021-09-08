5643b01793e87844b840f0145848e6efcc279c1c
singularis <singularis314@gmail.com>
2021-09-08 15:08:04 +0300

    Fixed bugs

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -7,20 +7,42 @@ if [ -e /etc/redhat-release ] ; then
   REDHAT_BASED=true
 fi
 
+sudo useradd ubuntu
+su ubuntu
+cd ~
+
 PACKER_VERSION="1.2.4"
 # create new ssh key
 [[ ! -f /home/ubuntu/.ssh/mykey ]] \
 && mkdir -p /home/ubuntu/.ssh \
 && ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
-&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh 
 
 # install packages
 if [ ${REDHAT_BASED} ] ; then
   yum -y update
-  yum install -y docker ansible unzip wget git bashcompletion
+  yum install -y unzip wget git bashcompletion
+  git clone https://github.com/sandervanvugt/kubernetes.git
+  cd kubernetes/
+  sudo ./setup-docker.sh 
+  sudo ./setup-kubetools.sh 
+  kubeadm init
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  kubectl create -f https://docs.projectcalico.org/manifests/calico.yaml
 else 
-  apt-get update
-  apt-get -y install docker.io ansible unzip python3-pip
+  apt-get -y update
+  apt-get install -y unzip wget git bashcompletion
+  git clone https://github.com/sandervanvugt/kubernetes.git
+  cd kubernetes/
+  sudo ./setup-docker.sh 
+  sudo ./setup-kubetools.sh 
+  kubeadm init
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  kubectl create -f https://docs.projectcalico.org/manifests/calico.yaml
 fi
 # add docker privileges
 usermod -G docker ubuntu
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -7,31 +7,36 @@ if [ -e /etc/redhat-release ] ; then
   REDHAT_BASED=true
 fi
 
+sudo useradd ubuntu
+su ubuntu
+cd ~
+
+
 PACKER_VERSION="1.2.4"
 # create new ssh key
 [[ ! -f /home/ubuntu/.ssh/mykey ]] \
-&& mkdir -p /home/vagrant/.ssh \
-&& ssh-keygen -f /home/vagrant/.ssh/mykey -N '' \
-&& chown -R vagrant:vagrant /home/vagrant/.ssh 
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh 
 
 # install packages
 if [ ${REDHAT_BASED} ] ; then
   yum -y update
-  yum install -y docker ansible unzip wget git bashcompletion
+  yum install -y unzip wget git bashcompletion
   git clone https://github.com/sandervanvugt/kubernetes.git
   cd kubernetes/
   sudo ./setup-docker.sh 
   sudo ./setup-kubetools.sh 
 else 
   apt-get -y update
-  apt-get install -y docker ansible unzip wget git bashcompletion
+  apt-get install -y   unzip wget git bashcompletion
   git clone https://github.com/sandervanvugt/kubernetes.git
   cd kubernetes/
   sudo ./setup-docker.sh 
   sudo ./setup-kubetools.sh 
 fi
 # add docker privileges
-usermod -G docker vagrant
+usermod -G docker ubuntu
 
 # clean up
 if [ ! ${REDHAT_BASED} ] ; then
