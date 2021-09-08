41c212ca908d2ec9154be33d0089e602abe508d1
singularis <singularis314@gmail.com>
2021-09-08 15:01:01 +0300

    Delete .vagrant directory

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["ffi",[">= 1.0.0"]],["win32-process",[">= 0"]],["vagrant-sshfs",["= 1.3.6"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"89d9fa7e045b74044b2c6ae26db9d94468a6f8848a7554a186dc013259f0d07e","vagrant_version":"2.2.18"}
